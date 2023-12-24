ab8acc241b560bad7392f214a73bf688a5c69093
David L. Inman <davidlevere@gmail.com>
2023-12-24 18:04:42 -0500

    particle stuff

--- src/drawable/particle.lua
+++ src/drawable/particle.lua
@@ -1,8 +1,36 @@
 
-local function manager()
+local function manager_add_particle(self, particle)
+    assert(type(particle) == "table" and particle.type == "particle")
+
+    local t = self.particles[particle.owner_id]
+    if t then
+        t[#t+1] = particle
+    else
+        self.particles[particle.owner_id] = { particle }
+    end
 end
 
-local function particle()
+local function manager_delete_particles(self, owner_id)
+    self.particles[owner_id] = nil
+end
+
+local function manager()
+    return { type = "particle_manager"
+           , particles = {}
+           , add = manager_add_particle
+           }
+end
+
+local function particle(owner_id, vec_path, color)
+    assert(type(owner_id) == "table")
+    assert(type(vec_path) == "function")
+    assert(color) 
+
+    return { type = "particle"
+           , owner_id = owner_id
+           , vec_path = vec_path
+           , color = color
+           }
 end
 
 return { manager = manager 
