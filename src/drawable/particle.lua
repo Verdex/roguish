bb575b068d6aa407cd59ac477a3b6c2afab0e0a8
David L. Inman <davidlevere@gmail.com>
2023-12-24 20:20:02 -0500

    inc prog

--- src/drawable/particle.lua
+++ src/drawable/particle.lua
@@ -1,4 +1,6 @@
 
+local vec = require "util/vec"
+
 local function manager_add_particle(self, particle)
     assert(type(particle) == "table" and particle.type == "particle")
 
@@ -14,22 +16,56 @@ local function manager_delete_particles(self, owner_id)
     self.particles[owner_id] = nil
 end
 
+local function manager_update(self, delta) -- TODO collision
+    for _, p_list in pairs(self.particles) do 
+        for _, p in ipairs(p_list) do
+            local _, r, g, b, a = p.color_path(delta)
+            local _, x, y = p.vec_path(delta)
+            self.r = r
+            self.g = g 
+            self.b = b
+            self.a = a
+            self.location.x = x
+            self.location.y = y
+        end
+    end
+end
+
+local function manager_draw(self) -- TODO virtual space to screen space transform
+    for _, p_list in pairs(self.particles) do 
+        for _, p in ipairs(p_list) do
+            p.drawer(p)
+        end
+    end
+end
+
 local function manager()
     return { type = "particle_manager"
            , particles = {}
            , add = manager_add_particle
+           , update = manager_update
+           , draw = manager_draw
            }
 end
 
-local function particle(owner_id, vec_path, color)
+local function particle(owner_id, drawer, vec_path, color_path)
     assert(type(owner_id) == "table")
     assert(type(vec_path) == "function")
-    assert(color) 
+    assert(type(color_path) == "function")
+
+    local _, x, y = vec_path(0)
+    local _, r, g, b, a = color_path(0)
 
     return { type = "particle"
            , owner_id = owner_id
+           , location = vec.vec2(x, y)
            , vec_path = vec_path
-           , color = color
+           , r = r
+           , g = g
+           , b = b
+           , a = a
+           , color_path = color_path
+           , drawer = drawer
            }
 end
 
